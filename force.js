eed82fb74c82081f654f4dcd3bb3c6d54582ed44
Alexander Mordvintsev <zzznah@gmail.com>
2026-02-06 15:16:13 +0100

    alice

--- force.js
+++ force.js
@@ -33,42 +33,40 @@ function prepareWASM(instance) {
     return main;
 }
 
-function forceLink({distance=20, iterations=3, strength=0.4}={}) {
-    function force(layout) {
-        const {nodes, pos, vel} = layout;
-        
-        for (let k=0; k<iterations; ++k) {
-            for (let i=0; i<nodes.length; ++i) {
-                for (const j of nodes[i]) {
-                    if (i > j) continue;
-                    let x = pos[j*3]   + vel[j*3]   - pos[i*3]   - vel[i*3];
-                    let y = pos[j*3+1] + vel[j*3+1] - pos[i*3+1] - vel[i*3+1];
-                    let z = pos[j*3+2] + vel[j*3+2] - pos[i*3+2] - vel[i*3+2];
-                    let l = Math.max(Math.hypot(x, y, z), 1.0);
-                    l = (l - distance) / l * strength;
-                    x *= l, y *= l, z *= l;
-                    vel[j*3] -= x; vel[j*3+1] -= y; vel[j*3+2] -= z;
-                    vel[i*3] += x; vel[i*3+1] += y; vel[i*3+2] += z;
-                }
-            }
-        }
-    }
-    return force;
-}
-
 class GraphLayout {
-    constructor(nodes, wasm, dim=2) {
-        this.nodes = nodes;
+    constructor(graph, wasm, dim=2) {
+        this.graph = graph;
+        this.nodes = graph.nodes;
         this.wasm = prepareWASM(wasm);
         this.dim = dim;
         this.velocityDecay = 0.1;
         this.pointN = 0;
         this.pos = new Float32Array(MaxPointN*3);
         this.vel = new Float32Array(MaxPointN*3);
-        this.linkForce = forceLink();
 
         this.chargeStrength = -3.0;
         this.chargeMaxDist = 2000;
+
+        this.linkIterN = 2;
+        this.linkDistance = 25;
+        this.linkStrength = 0.5;
+    }
+
+    linkForce() {
+        const {graph, pos, vel, linkStrength, linkDistance} = this;
+        const links = this.graph.links;
+
+        for (let p=0; p<links.length; p+=2) {
+            const i=links[p], j=links[p+1];
+            let x = pos[j*3]   + vel[j*3]   - pos[i*3]   - vel[i*3];
+            let y = pos[j*3+1] + vel[j*3+1] - pos[i*3+1] - vel[i*3+1];
+            let z = pos[j*3+2] + vel[j*3+2] - pos[i*3+2] - vel[i*3+2];
+            let l = Math.max(Math.hypot(x, y, z), 1.0);
+            l = (l - linkDistance) / l * linkStrength;
+            x *= l, y *= l, z *= l;
+            vel[j*3] -= x; vel[j*3+1] -= y; vel[j*3+2] -= z;
+            vel[i*3] += x; vel[i*3+1] += y; vel[i*3+2] += z;
+        }
     }
 
     chargeForce() {
@@ -101,29 +99,26 @@ class GraphLayout {
     }
 
     tick(step_n=1) {
-        const {pos, vel} = this;
+        const {pos, vel, dim} = this;
         const f = 1.0-this.velocityDecay;
-        const rnd = ()=>(Math.random()-0.5)*10.0;
+        const rnd = ()=>(Math.random()-0.5);
         // init new points
-        for (let i=this.pointN; i<this.nodes.length; ++i) {
+        for (let i=this.pointN; i<this.graph.nodeN; ++i) {
             let nn = 0;
-            let x=0, y=0, z=0;
+            let x=rnd(), y=rnd(), z=dim==3 ? rnd() : 0.0;
             for (const j of this.nodes[i]) {
                 if (j>=this.pointN) continue; // skip new points
                 let p=j*3; nn++;
                 x+=this.pos[p++]; y+=this.pos[p++]; z+=this.pos[p];
             }
-            if (nn>0) {
-                x/=nn; y/=nn; z/=nn;
-            } else {
-                x=rnd(); y=rnd(); z=this.dim==3 ? rnd() : 0.0;
-            }
+            if (nn>1) {x/=nn; y/=nn; z/=nn;}
             let p=i*3; this.pos[p++]=x; this.pos[p++]=y; this.pos[p]=z;
         }
-        this.pointN = this.nodes.length;
+        this.pointN = this.graph.nodeN;
 
+        // apply forces
         for (let k=0; k<step_n; ++k) {
-            this.linkForce(this);
+            for (let i=0; i<this.linkIterN; ++i) this.linkForce();
             this.chargeForce();
             for (let i=0; i<this.pointN*3; ++i) {
                 pos[i] += vel[i];
--- graph.js
+++ graph.js
@@ -10,6 +10,7 @@ const calcCases = (nodes, states)=>nodes.map((node, i)=>{
 class GrowingGraph {
     constructor(rule=2182) {
         this.rule = rule;
+        this.flipProb = 0.0;
         this.lastGen = 0;
         this.nodes = [[9,1,2],[0,2,4],[1,3,0],[2,4,6],[3,5,1],
                       [4,6,8],[5,7,3],[6,8,9],[7,9,5],[8,0,7]];
@@ -20,9 +21,7 @@ class GrowingGraph {
         this.links = this.updateLinks();
     }
 
-    get nodeN() {
-        return this.nodes.length;
-    }
+    get nodeN() {return this.nodes.length;}
 
     updateLinks() {
         const links = [];
@@ -41,16 +40,15 @@ class GrowingGraph {
     }
 
     grow() {
-        const {nodes, states, rule} = this;
+        const {nodes, states, rule, flipProb} = this;
 
         if (this.phase === 0) { // update states
             calcCases(nodes, states).forEach((r,i)=>{
                 this.states[i] = (rule >> r) & 1;
                 this.dividing[i] = (rule >> (r+CaseN)) & 1;
-                // const noise = Math.random();
-                // if (noise<0.00005) {
-                //     this.states[i] = 1-this.states[i];
-                // }
+                if (flipProb > 0.0 && Math.random() < flipProb) {
+                    this.states[i] = 1-this.states[i];
+                }
              });
         } else {
             ++this.lastGen;
